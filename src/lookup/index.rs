8fc58f4a46611aa31b1189ab6a654ac073c6618c
lima1909 <linke.mario@web.de>
2024-02-17 15:11:11 +0100

    remove max/min fields in IndexLookup

--- src/lookup/index.rs
+++ src/lookup/index.rs
@@ -26,12 +26,8 @@ pub type MultiPosIndex<X = usize> = IndexLookup<MultiKeyPositon<X>, X>;
 
 /// `Key` is from type [`usize`] and the information are saved in a List (Store).
 #[derive(Debug)]
-pub struct IndexLookup<P, X = usize> {
-    inner: Vec<Option<(usize, P)>>,
-    min_index: usize,
-    max_index: usize,
-    _x: PhantomData<X>,
-}
+#[repr(transparent)]
+pub struct IndexLookup<P, X = usize>(Vec<Option<(usize, P)>>, PhantomData<X>);
 
 impl<P, X> Lookup<usize> for IndexLookup<P, X>
 where
@@ -40,10 +36,10 @@ where
     type Pos = X;
 
     fn key_exist(&self, key: usize) -> bool {
-        matches!(self.inner.get(key), Some(Some(_)))
+        matches!(self.0.get(key), Some(Some(_)))
     }
     fn pos_by_key(&self, key: usize) -> &[Self::Pos] {
-        match self.inner.get(key) {
+        match self.0.get(key) {
             Some(Some((_, p))) => p.as_slice(),
             _ => &[],
         }
@@ -59,90 +55,29 @@ where
 
     fn insert(&mut self, key: Self::Key, pos: Self::Pos) {
         // if necessary (len <= idx), than double the vec
-        if self.inner.len() <= key {
+        if self.0.len() <= key {
             const PRE_ALLOC_SIZE: usize = 100;
-            self.inner.resize(key + PRE_ALLOC_SIZE, None);
+            self.0.resize(key + PRE_ALLOC_SIZE, None);
         }
 
         // insert new key and pos
-        match self.inner.get_mut(key) {
+        match self.0.get_mut(key) {
             Some(Some((_, p))) => p.add_pos(pos),
-            _ => self.inner[key] = Some((key, P::new(pos))),
-        }
-
-        // define new max index
-        if self.max_index < key {
-            self.max_index = key;
-        }
-        // define new min index
-        if self.min_index > key {
-            self.min_index = key;
+            _ => self.0[key] = Some((key, P::new(pos))),
         }
     }
 
     fn delete(&mut self, key: Self::Key, pos: &Self::Pos) {
-        if let Some(Some((_, rm_idx))) = self.inner.get_mut(key) {
+        if let Some(Some((_, rm_idx))) = self.0.get_mut(key) {
             if rm_idx.remove_pos(pos) {
                 // las pos was deleted
-                self.inner[key] = None;
-
-                // define new max index
-                if self.max_index == key {
-                    self.max_index = self.find_new_max_index()
-                }
-                if self.min_index == key {
-                    self.min_index = self.find_new_min_index()
-                }
+                self.0[key] = None;
             }
         }
     }
 
     fn with_capacity(capacity: usize) -> Self {
-        Self {
-            inner: Vec::with_capacity(capacity),
-            min_index: usize::MAX,
-            max_index: 0,
-            _x: PhantomData,
-        }
-    }
-}
-
-//
-// ----------- internal (private) helper implementation --------------------------
-//
-impl<P, X> IndexLookup<P, X> {
-    #[inline(always)]
-    fn find_new_max_index(&self) -> usize {
-        self.inner
-            .iter()
-            .enumerate()
-            .rev()
-            .find_map(|(idx, o)| {
-                if o.is_some() {
-                    return Some(idx);
-                }
-                None
-            })
-            .unwrap_or_default()
-    }
-
-    #[inline(always)]
-    fn find_new_min_index(&self) -> usize {
-        self.inner
-            .iter()
-            .enumerate()
-            .find_map(|(idx, o)| {
-                if o.is_some() {
-                    return Some(idx);
-                }
-                None
-            })
-            .unwrap_or(usize::MAX)
-    }
-
-    #[inline(always)]
-    pub fn get_key_by_index(&self, index: usize) -> Option<usize> {
-        self.inner.get(index)?.as_ref().map(|(k, _)| *k)
+        Self(Vec::with_capacity(capacity), PhantomData)
     }
 }
 
@@ -164,23 +99,30 @@ impl<P, X> Deref for IndexLookup<P, X> {
 impl<P, X> IndexLookupExt<P, X> {
     /// Returns all stored `Key`s.
     pub fn keys(&self) -> impl Iterator<Item = usize> + '_ {
-        if self.0.min_index > self.0.max_index {
-            &[]
-        } else {
-            &self.0.inner[self.0.min_index..=self.0.max_index]
-        }
-        .iter()
-        .filter_map(|o| o.as_ref().map(|(key, _)| *key))
+        self.0
+             .0
+            .iter()
+            .filter_map(|o| o.as_ref().map(|(key, _)| *key))
     }
 
     /// Returns smallest stored `Key`.
     pub fn min_key(&self) -> Option<usize> {
-        self.0.get_key_by_index(self.0.min_index)
+        self.0 .0.iter().find_map(|o| {
+            if let Some((idx, _)) = o {
+                return Some(*idx);
+            }
+            None
+        })
     }
 
     /// Returns greatest stored `Key`.
     pub fn max_key(&self) -> Option<usize> {
-        self.0.get_key_by_index(self.0.max_index)
+        self.0 .0.iter().rev().find_map(|o| {
+            if let Some((idx, _)) = o {
+                return Some(*idx);
+            }
+            None
+        })
     }
 }
 
@@ -196,7 +138,7 @@ where
     where
         It: IntoIterator<Item = Self::Key>,
     {
-        let mut lkup = IndexLookup::<P, X>::with_capacity(self.inner.len());
+        let mut lkup = IndexLookup::<P, X>::with_capacity(self.0.len());
 
         for key in keys {
             for p in self.pos_by_key(key) {
@@ -285,47 +227,35 @@ mod tests {
             // both not set
             assert_eq!(None, idx.min_key());
             assert_eq!(None, idx.max_key());
-            assert_eq!(usize::MAX, idx.min_index);
-            assert_eq!(0, idx.max_index);
             assert!(idx.keys().next().is_none());
 
             // first insert, max and min are equal
             idx.insert(1, 0);
             assert_eq!(Some(1), idx.min_key());
             assert_eq!(Some(1), idx.max_key());
-            assert_eq!(1, idx.min_index);
-            assert_eq!(1, idx.max_index);
             assert_eq!(vec![1], idx.keys().collect::<Vec<_>>());
 
             // first insert, max and min are equal
             idx.insert(10, 1);
             assert_eq!(Some(1), idx.min_key());
             assert_eq!(Some(10), idx.max_key());
-            assert_eq!(1, idx.min_index);
-            assert_eq!(10, idx.max_index);
             assert_eq!(vec![1, 10], idx.keys().collect::<Vec<_>>());
 
             idx.insert(11, 2);
             assert_eq!(Some(1), idx.min_key());
             assert_eq!(Some(11), idx.max_key());
-            assert_eq!(1, idx.min_index);
-            assert_eq!(11, idx.max_index);
             assert_eq!(vec![1, 10, 11], idx.keys().collect::<Vec<_>>());
 
             idx.delete(10, &1);
             idx.delete(11, &2);
             assert_eq!(Some(1), idx.min_key());
             assert_eq!(Some(1), idx.max_key());
-            assert_eq!(1, idx.min_index);
-            assert_eq!(1, idx.max_index);
             assert_eq!(vec![1], idx.keys().collect::<Vec<_>>());
 
             // remove last
             idx.delete(1, &0);
             assert_eq!(None, idx.min_key());
             assert_eq!(None, idx.max_key());
-            assert_eq!(usize::MAX, idx.min_index);
-            assert_eq!(0, idx.max_index);
             assert!(idx.keys().next().is_none());
         }
 
@@ -347,16 +277,12 @@ mod tests {
             idx.insert(4, 4);
             assert_eq!(Some(1), idx.min_key());
             assert_eq!(Some(4), idx.max_key());
-            assert_eq!(1, idx.min_index);
-            assert_eq!(4, idx.max_index);
             assert_eq!(vec![1, 4], idx.keys().collect::<Vec<_>>());
 
             // new min
             idx.insert(0, 0);
             assert_eq!(Some(0), idx.min_key());
             assert_eq!(Some(4), idx.max_key());
-            assert_eq!(0, idx.min_index);
-            assert_eq!(4, idx.max_index);
             assert_eq!(vec![0, 1, 4], idx.keys().collect::<Vec<_>>());
 
             // new max
